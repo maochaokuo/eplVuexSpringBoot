c0e8a63d6c08e7b7779ccbdbe8cfe3ec81a9fc5d
Thomas Mao <tm1813@gmail.com>
2021-04-21 22:49:35 +0800

    re add eplSpringBootApi project

---
+++ eplSpringBootApi/src/main/java/com/epl/eplspringbootapi/EplSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.epl.eplspringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EplSpringBootApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EplSpringBootApiApplication.class, args);
+    }
+
+}
---
+++ eplSpringBootApi/src/test/java/com/epl/eplspringbootapi/EplSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epl.eplspringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EplSpringBootApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
